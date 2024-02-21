519f2a7b1719a57623dae9fd047a4955922cf6c5
theflashwin <theflashwin1@gmail.com>
2024-02-21 13:33:47 -0500

    Initialized Java Android Files

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS2340A_Team21"
+include(":app")
