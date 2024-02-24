e5c779a49640473f281a0f4ec20e05a44a38460a
theflashwin <theflashwin1@gmail.com>
2024-02-24 17:59:31 -0500

    Before adding google-servcies:

---
+++ app/src/main/java/com/example/cs2340a_team21/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs2340a_team21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+    }
+}
