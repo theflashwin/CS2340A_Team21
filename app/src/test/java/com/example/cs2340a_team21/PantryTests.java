fe1c2daf53d9ebdee9d22d5fdbb80939586a1609
ygurude <ygurude@gmail.com>
2024-03-31 23:11:16 -0400

    Added 10 Tests Looking to add more

--- app/src/test/java/com/example/cs2340a_team21/PantryTests.java
+++ app/src/test/java/com/example/cs2340a_team21/PantryTests.java
@@ -45,10 +45,44 @@ public class PantryTests {
         assertEquals("null", IngredientsViewModel.addIngredient("Strawberries", null, "25", "01/31"));
     }
 
-//    @Test
-//    public void testNullEntry() {
-//        assertEquals("null", IngredientsViewModel.addIngredient("Strawberries", null, "25", "01/31");
-//    }
+    @Test
+    public void testEmptyEntry() {
+        assertEquals("null", IngredientsViewModel.addIngredient("", "2", "50", "N/A"));
+    }
+
+    @Test
+    public void testSuccessfulEntry() {
+        assertEquals("success", IngredientsViewModel.addIngredient("Protein Powder", "1", "50", "N/A"));
+    }
+
+    @Test
+    public void testGetIngredients() {
+        assertTrue(IngredientsViewModel.getIngredients().isEmpty());
+    }
+
+    @Test
+    public void testGetIngredientsAfterAdding() {
+        IngredientsViewModel.addIngredient("Strawberries", "10", "20", "N/A");
+        assertFalse(IngredientsViewModel.getIngredients().isEmpty());
+    }
+
+    @Test
+    public void testAddMultipleIngredients() {
+
+        IngredientsViewModel.addIngredient("Flour", "100", "0", "N/A");
+        IngredientsViewModel.addIngredient("Sugar", "200", "300", "N/A");
+        IngredientsViewModel.addIngredient("Oil", "300", "400", "N/A");
+        assertEquals(3, IngredientsViewModel.getIngredients().size());
+    }
+
+    @Test
+    public void testRemoveIngredient() {
+        IngredientsViewModel.addIngredient("Protein Powder", "50", "50", "N/A");
+        assertTrue(IngredientsViewModel.getIngredients().size() > 0);
+        IngredientsViewModel.getIngredients().remove(0);
+        assertTrue(IngredientsViewModel.getIngredients().isEmpty());
+    }
+
 
 
 }
