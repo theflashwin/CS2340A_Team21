c3e6f4daff8f94aa4dc1c7d9364ab0823bbe9a9d
theflashwin <theflashwin1@gmail.com>
2024-03-31 23:17:11 -0400

    Added recipe stuff

---
+++ app/src/main/java/com/example/cs2340a_team21/views/RecipeAdapter.java
@@ -0,0 +1,72 @@
+package com.example.cs2340a_team21.views;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cs2340a_team21.R;
+import com.example.cs2340a_team21.objects.Ingredient;
+import com.example.cs2340a_team21.objects.Recipe;
+
+import java.util.List;
+
+import org.w3c.dom.Text;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    List<Recipe> recipes;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView name;
+        private final Button open;
+
+        public ViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.recipeName);
+            open = view.findViewById(R.id.openButton);
+        }
+
+        public TextView getName() {
+            return name;
+        }
+
+        public TextView getOpen() {
+            return open;
+        }
+
+    }
+
+    public RecipeAdapter(List<Recipe> data) {
+
+        this.recipes = data;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recipe_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Recipe r = recipes.get(position);
+        viewHolder.getName().setText(r.name);
+
+        String canOpen = "hey";
+
+        viewHolder.getOpen().setText(canOpen);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+
+}
